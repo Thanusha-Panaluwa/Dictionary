87bb286802dc883e13d4dbfe3d5af6d44f1984dc
Thanusha Panaluwa <thanushapanaluwa@gmail.com>
2024-08-30 14:12:36 +0800

    Initiate the site

---
+++ my_dictionary/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Dictionary from "./Dictionary";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p>
+          Hello! I am Thanusha.
+        </p>     
+      </header>
+      <Dictionary />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_dictionary/src/Dictionary.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dictionary(){
+    return(
+        <div className="dictionary">
+            <p>
+                Hello!
+            </p>
+        </div>);
+
+}
